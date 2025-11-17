4c7a3bdb5ba1a9a4ce6d05e1f917707d138cf1f7
aissamed7 <contacteinfo71@gmail.com>
2025-11-17 20:16:41 +0300

    feat: Add react-grid-layout for dashboard widgets
    Integrates react-grid-layout to enable a draggable and resizable widget interface for the dashboard.
    
    Updates Tailwind CSS configuration to include relevant directories for component styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx",
+    "./{components,data,hooks,i18n,pages,services}/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
